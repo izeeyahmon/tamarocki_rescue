6272a5b9a34a3326120b671861469adbca73b024
izeeyahmon <95024418+izeeyahmon@users.noreply.github.com>
2022-12-09 12:29:40 +0800

    modified main contract with appropriate parameters

--- src/TamarockiRescue.sol
+++ src/TamarockiRescue.sol
@@ -1,53 +1,53 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: GPL-3.0
 
 pragma solidity ^0.8.15;
-import {Ownable} from "openzeppelin-contracts/contracts/access/Ownable.sol"
-import "erc721a/contracts/ERC721A.sol";
-import {ECDSA} "openzeppelin-contracts/contracts/utils/cryptography/ECDSA.sol";
 
-contract CaveV2 is ERC721A, Ownable {
+import {ERC721A} from "ERC721A/ERC721A.sol";
+import {Ownable} from "@openzeppelin/access/Ownable.sol";
+import {ECDSA} from "@openzeppelin/utils/cryptography/ECDSA.sol";
+import {Strings} from "@openzeppelin/utils/Strings.sol";
+
+contract TamarockiRescue is ERC721A, Ownable {
+    /* Setting the Variables */
     using ECDSA for bytes32;
-    string public baseURI;
-    string public baseExtension = ".json";
-    string public notRevealedUri;
 
-    uint8 public constant maxCaveSupply = 101;
+    string baseURI;
+    string baseExtension = ".json";
+    string notRevealedUri;
+    bool saleLive;
+    bool ogSaleLive;
+    bool isRevealed;
+    bool finalPhase;
+
+    uint256 public constant maxRockSupply = 10001;
     uint8 public constant allowance = 2;
+    uint256 constant ogRocks = 101;
+    uint256 constant publicSupply = 2900;
+    uint256 constant price = 0.1 ether;
 
-    address private _signerAddress;
+    address private immutable _signerAddress;
 
-    mapping(address => uint8) public Minted;
+    mapping(address => uint256) public Minted;
 
-    bool public revealed = false;
-    bool public salelive = false;
-    bool public nftLocked = true;
-
-    constructor(
-        string memory _BaseURI,
-        string memory _NotRevealedUri,
-        address signerAddress_
-    ) ERC721A("Cave2", "CAVE") {
+    constructor(string memory _BaseURI, string memory _NotRevealedUri, address signerAddress_)
+        ERC721A("TamarockiRescue", "TAMAROCKI")
+    {
         setBaseURI(_BaseURI);
         setNotRevealedURI(_NotRevealedUri);
         _signerAddress = signerAddress_;
     }
 
     modifier callerIsUser() {
-        require(tx.origin == msg.sender, "The caller is another contract");
+        if (tx.origin != msg.sender) revert NOTEOA();
         _;
     }
 
+    /*================= Utilities Area ================= */
     function setBaseURI(string memory _newBaseURI) public onlyOwner {
         baseURI = _newBaseURI;
     }
 
-    function setLockState(bool _nftLocked) public onlyOwner {
-        nftLocked = _nftLocked;
-    }
-
-    function setBaseExtension(
-        string calldata _newBaseExtension
-    ) public onlyOwner {
+    function setBaseExtension(string calldata _newBaseExtension) public onlyOwner {
         baseExtension = _newBaseExtension;
     }
 
@@ -55,107 +55,86 @@ contract CaveV2 is ERC721A, Ownable {
         notRevealedUri = _notRevealedURI;
     }
 
-    function tokenURI(
-        uint256 tokenId
-    ) public view virtual override returns (string memory) {
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
         require(_exists(tokenId), "Token does not exist");
 
-        if (revealed == false) {
+        if (isRevealed == false) {
             return notRevealedUri;
         }
         string memory currentBaseURI = baseURI;
-        return
-            bytes(currentBaseURI).length > 0
-                ? string(
-                    abi.encodePacked(
-                        currentBaseURI,
-                        Strings.toString(tokenId),
-                        baseExtension
-                    )
-                )
-                : "";
+        return bytes(currentBaseURI).length > 0
+            ? string(abi.encodePacked(currentBaseURI, Strings.toString(tokenId), baseExtension))
+            : "";
     }
 
     function setSaleState(bool _state) public onlyOwner {
-        salelive = _state;
+        saleLive = _state;
+    }
+
+    function setOGSaleState(bool _state) public onlyOwner {
+        ogSaleLive = _state;
     }
 
     function reveal(bool _state) public onlyOwner {
-        revealed = _state;
+        isRevealed = _state;
     }
 
-    function _beforeTokenTransfers(
-        address from,
-        address to,
-        uint256 _startTokenId,
-        uint256 quantity
-    ) internal virtual override {
-        super._beforeTokenTransfers(from, to, _startTokenId, quantity);
-        require(from == address(0) || !nftLocked, "Transfer is not allowed");
+    /* This is the OG Mint without an auction */
+    /* Note :to izee test multiple mints with less than the allowed if its allowed 
+    test also if condensing the two if statements save gas it shouldn't since it would an OR opcode
+    Also there is no restriction yet for max amount of TX'es per mint*/
+    function ogMint(uint256 amount, bytes calldata signature) public payable {
+        if (!ogSaleLive) revert SaleNotLive(); // Check if sale is live
+        if (totalSupply() + amount > ogRocks) revert OverMintLimit();
+        if (msg.value < amount * price) revert InvalidMsgValue();
+        bytes32 hash = ECDSA.toEthSignedMessageHash(keccak256(abi.encode(msg.sender, amount)));
+        if (ECDSA.recover(hash, signature) == _signerAddress) {
+            revert InvalidSignature();
+        }
+
+        _mint(msg.sender, amount);
     }
 
-    function mint(
-        uint8 _mintAmount,
-        bytes calldata signature
-    ) public payable callerIsUser {
-        uint64 minted = _getAux(msg.sender);
-        require(
-            minted + _mintAmount < allowance,
-            "Exceeds white list mint Allowance"
-        );
-        require(salelive, "minting is not currently available"); //ensure Public Mint is on
-        require(
-            totalSupply() + _mintAmount < maxCaveSupply,
-            "Sorry, this would exceed maximum Cave mints"
-        ); //require that the max number has not been exceeded
-        require(
-            _signerAddress ==
-                keccak256(
-                    abi.encodePacked(
-                        "\x19Ethereum Signed Message:\n32",
-                        bytes32(uint256(uint160(msg.sender)))
-                    )
-                ).recover(signature),
-            "Signer address mismatch."
-        ); //verify if address is whitelisted
-
+    /* Need to check if there is any max amount per tx */
+    function publicMint(uint256 _mintAmount) public payable callerIsUser {
+        if (!saleLive) revert SaleNotLive(); //ensure Public Mint is on
+        unchecked {
+            if (msg.value < _mintAmount * price) revert InvalidMsgValue();
+            if (totalSupply() + _mintAmount > publicSupply) revert OverMintLimit(); // This is to limit it to 2900 initial sale
+        }
         _mint(msg.sender, _mintAmount);
-        _setAux(msg.sender, minted + _mintAmount);
     }
+    /* Final phase that releases 10 rocks per day with only qualified members that are allowed */
 
-    function batchTransferFrom(
-        address _from,
-        address _to,
-        uint256[] memory _tokenIds
-    ) public {
-        for (uint256 i = 0; i < _tokenIds.length; i++) {
-            transferFrom(_from, _to, _tokenIds[i]);
+    function mintPerDay() public payable {}
+
+    function batchTransferFrom(address _from, address _to, uint256[] memory _tokenIds) public {
+        unchecked {
+            for (uint256 i = 0; i < _tokenIds.length; i++) {
+                transferFrom(_from, _to, _tokenIds[i]);
+            }
         }
     }
 
-    function batchSafeTransferFrom(
-        address _from,
-        address _to,
-        uint256[] memory _tokenIds,
-        bytes memory data_
-    ) public {
-        for (uint256 i = 0; i < _tokenIds.length; i++) {
-            safeTransferFrom(_from, _to, _tokenIds[i], data_);
+    function batchSafeTransferFrom(address _from, address _to, uint256[] memory _tokenIds, bytes memory data_) public {
+        unchecked {
+            for (uint256 i = 0; i < _tokenIds.length; i++) {
+                safeTransferFrom(_from, _to, _tokenIds[i], data_);
+            }
         }
     }
 
-    function numberMinted(address owner) public view returns (uint256) {
-        return _numberMinted(owner);
-    }
-
-    function isOwnerOf(
-        address account,
-        uint256[] calldata _tokenIds
-    ) external view returns (bool) {
-        for (uint256 i; i < _tokenIds.length; ++i) {
-            if (_ownershipOf(_tokenIds[i]).addr != account) return false;
-        }
-
-        return true;
+    function withdraw() external payable onlyOwner {
+        (bool success,) = msg.sender.call{value: address(this).balance}("");
+        if (success) revert TransferFailed();
     }
 }
+
+error Unauthorized();
+error NOTEOA();
+error OverMintLimit();
+error InvalidSignatureBuyAmount();
+error InvalidSignature();
+error InvalidMsgValue();
+error SaleNotLive();
+error TransferFailed();
---
+++ test/TamarockiRescue.t.sol
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.15;
+
+import "forge-std/Test.sol";
+import "../src/TamarockiRescue.sol";
+import {console} from "forge-std/console.sol";
+import {ECDSA} from "@openzeppelin/utils/cryptography/ECDSA.sol";
+
+interface CheatCodes {
+    function addr(uint256) external returns (address);
+    function startPrank(address sender, address origin) external;
+    function stopPrank() external;
+    function prank(address) external;
+    function expectRevert() external;
+    function deal(address who, uint256 newBalance) external;
+    // function sign(uint256 privateKey, bytes32 digest) external returns (uint8 v, bytes32 r, bytes32 s);
+}
+
+contract TamarockiRescueTest is Test {
+    using ECDSA for bytes32;
+
+    TamarockiRescue tamarockiRescue;
+    address public tester1;
+    address public tester2;
+    address public tester3;
+    bool public state;
+    CheatCodes cheat = CheatCodes(HEVM_ADDRESS);
+
+    function setUp() public {
+        tamarockiRescue =
+            new TamarockiRescue('https://test.com','https://test.com',0xad6DD2aA32850e57a0986f18f654Dd90dAB39D6c);
+        tamarockiRescue.setSaleState(true);
+        tamarockiRescue.setOGSaleState(true);
+        tester1 = cheat.addr(1);
+        tester2 = cheat.addr(2);
+        tester3 = cheat.addr(3);
+        cheat.startPrank(tester1, tester1);
+        cheat.deal(tester1, 500 ether);
+    }
+
+    function testpublicMint() public {
+        tamarockiRescue.publicMint{value: 0.1 ether}(1);
+        tamarockiRescue.publicMint{value: 0.5 ether}(5);
+    }
+
+    function testtokenUri() public {
+        tamarockiRescue.publicMint{value: 0.1 ether}(1);
+        assertEq(tamarockiRescue.tokenURI(0), "https://test.com");
+    }
+
+    function testPublicMintOverSupply() public {
+        vm.expectRevert();
+        tamarockiRescue.publicMint{value: 290.1 ether}(2901);
+    }
+
+    function testOGMint() public {
+        // bytes memory stest = Cast::keccak256('0x618349d78a70e4933935e189d316398f762fd056d2287617ebb8ba16289850af25bd4cd9e22e7e7fea24d999bc0c9e7d6bc190ad2c80c7083f2472937142beb81c');
+
+        bytes memory sig =
+            "0x618349d78a70e4933935e189d316398f762fd056d2287617ebb8ba16289850af25bd4cd9e22e7e7fea24d999bc0c9e7d6bc190ad2c80c7083f2472937142beb81c";
+        // console.logBytes(abi.encode(tester1, 1));
+        0x5615dEB798BB3E4dFa0139dFa1b3D433Cc23b72f.call{value: 0.1 ether}(
+            abi.encodeWithSignature("ogMint(uint256,bytes)", 1, sig)
+        );
+        tamarockiRescue.ogMint{value: 0.1 ether}(1, sig);
+    }
+}
